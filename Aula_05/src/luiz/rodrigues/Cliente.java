e05cc929eb80051a08530f6ff78acc2e26bf6f17
RatonesR <lfrodrigues703@gmail.com>
2021-05-21 18:28:11 -0300

    Aula_05

---
+++ Aula_05/src/luiz/rodrigues/Cliente.java
@@ -0,0 +1,20 @@
+package luiz.rodrigues;
+
+public class Cliente {
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
+}
